2f2df68719c78d5dfbc2cc13885ad89e15ee723f
Coder2195 <Coder2195Mail@gmail.com>
2022-11-03 15:41:28 -0400

    identify only

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,41 +1,42 @@
-import NextAuth, { Session, Token, User } from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
+import NextAuth, { Session, Token, User } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 import { signIn } from "next-auth/react";
 
-
 export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_ID,
-            clientSecret: process.env.DISCORD_SECRET,
-        }),
-        // ...add more providers here
-    ],
-    callbacks: {
-        async session({ session, token }) {
-            session.accessToken = token.accessToken;
-            if (session?.user) {
-                session.user = {
-                    ...token.user, ...session.user
-                }
-            }
-            return session;
-        },
-        async jwt({ user, token, account }) {
-            if (user) {
-                token.user = user;
-            }
-            if (account) {
-                token.accessToken = account.access_token;
-            }
-            return token;
-        }
+  // Configure one or more authentication providers
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET,
+      authorization: "https://discord.com/api/oauth2/authorize?scope=identify",
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      if (session?.user) {
+        session.user = {
+          ...token.user,
+          ...session.user,
+        };
+      }
+      return session;
     },
-    session: {
-        strategy: "jwt",
+    async jwt({ user, token, account }) {
+      if (user) {
+        token.user = user;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
-}
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
 
 //@ts-ignore
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
