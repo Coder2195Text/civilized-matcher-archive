dbfa0a59f1aea3c76b533c1fc908a0e3368d6e51
Coder2195Text <63069499+Coder2195Text@users.noreply.github.com>
2022-11-22 09:13:03 -0500

    Update form.tsx

--- pages/form.tsx
+++ pages/form.tsx
@@ -110,8 +110,8 @@ export default function Form() {
             );
             return;
           }
-          if (!data?.preferredGenders.includes(String(data.age)) && !confirm("(AI Age Forget Detection): Do you really want to submit? You may have forgetten to put in your age. Press cancel to review your form.")){
-            setErrors(`May have forgetten to put in age, since you are an ${data.age} year old who prefers people of ages ${data.preferredAges.replaceAll(";", ", ")}`)
+          if (!data?.preferredGenders.includes(String(data?.age)) && !confirm("(AI Age Forget Detection): Do you really want to submit? You may have forgetten to put in your age. Press cancel to review your form.")){
+            setErrors(`May have forgetten to put in age, since you are an ${data?.age} year old who prefers people of ages ${data?.preferredAges.replaceAll(";", ", ")}`)
             return;
           }
           setSubmitting(true);
