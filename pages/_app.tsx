bd481586bb3c0069b7721cef00426adfad0f9de7
Coder2195 <Coder2195Mail@gmail.com>
2022-12-04 14:29:04 -0500

    react bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import "bootswatch/dist/minty/bootstrap.min.css";
+import SSRProvider from "react-bootstrap/SSRProvider";
 
 export default function App({
   Component,
@@ -10,8 +11,10 @@ export default function App({
 }: AppProps) {
   session = session as Session;
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <SSRProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </SSRProvider>
   );
 }
