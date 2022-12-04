a6cd9551d28f3398a492e5a610fbbd8649a62a82
Coder2195 <Coder2195Mail@gmail.com>
2022-12-04 11:55:34 -0500

    new changes to theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import "bootswatch/dist/minty/bootstrap.min.css";
 
 export default function App({
   Component,
